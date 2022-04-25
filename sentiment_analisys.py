7ec2ba290c54a30399d7c00f7b0c983bf43f0fde
Boscoaran <boscoarangurensaiz@gmail.com>
2022-04-25 12:06:31 +0200

    preproceso

--- sentiment_analisys.py
+++ sentiment_analisys.py
@@ -1,18 +1,22 @@
+import numpy as np
+from numpy import double
+import pandas as pd
 import string
 import nltk
 from nltk.stem import WordNetLemmatizer, PorterStemmer
 from nltk.corpus import stopwords
-from numpy import double
-import numpy as np
-import pandas as pd
+from nltk.tokenize import RegexpTokenizer
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import f1_score, classification_report, confusion_matrix
 from sklearn.neighbors import KNeighborsClassifier
-import time
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 import warnings
 warnings.filterwarnings("ignore")
 
+target_map={1: '0', 2: '0', 3: '1', 4: '2', 5: '2'}
+
 def normalize_dict(d):
+
     lemmatazier = WordNetLemmatizer()
     ps = PorterStemmer()
 
@@ -39,7 +43,9 @@ def normalize_dict(d):
         d[i]=dic_norm[d[i]]
     return (d)
     
+
 def normalize(d):
+
     lemmatazier = WordNetLemmatizer()
     ps = PorterStemmer()
     for n in range(len(d)):
@@ -59,56 +65,61 @@ def normalize(d):
         d[n]=s
     return (d)
 
-def main():
-    f='HRBlockIntuitReviewsTrainDev_vLast7.csv'
-    t='overall'
 
-    dataset=pd.read_csv(f)
-    del dataset['asin']
-    del dataset['Unnamed: 0']
-    del dataset['Unnamed: 5']
-    del dataset['reviewTime_y']
-    del dataset['reviewerName_y']
-    del dataset['Country']
-    del dataset['rank']
-    #del dataset['price']
-    del dataset['unixReviewTime']
-    del dataset['Postal Code']
-    del dataset['vote_y']
-    del dataset['verified_y']
-    del dataset['reviewerID_y']
+def preprocess(dataset):
 
-    dataset['price']=dataset['price'].astype(double).round(2)
-
-    text_features=['summary', 'title', 'desciption', 'details', 'reviewText']
+    text_features=['summary', 'title', 'description', 'details', 'reviewText']
     categorical_features=['brand', 'category', 'main_cat', 'State']
+    index_features=['Unnamed: 0', 'asin', 'reviewerName_y', 'reviewerID_y']
+    other_features=['Unnamed: 5', 'reviewTime_y', 'Country', 'verified_y']
 
+    #Eliminar features de índice, categóricas y no útiles
+    for feature in categorical_features:
+        del dataset[feature]  
+    for feature in index_features:
+        del dataset[feature]  
+    for feature in other_features:
+        del dataset[feature]   
+   
     nltk.data.path.append('nltk_data')
 
-    print('start')
+    #Clean text features
     dataset['title']=normalize_dict(dataset['title'])
-    print('Title normalized')
-    dataset['summary']=normalize(dataset['summary'])
-    print('Summary normalized')
+    print('Title normalizado')
     dataset['description']=normalize_dict(dataset['description'])
-    print('Description normalized')
+    print('Description normalizado')
     dataset['details']=normalize_dict(dataset['details'])
-    print('Details normalized')
+    print('Details normalizado')
+    dataset['summary']=normalize(dataset['summary'])
+    print('Summary normalizado')
     dataset['reviewText']=normalize(dataset['reviewText'])
-    print('Review text normalized')
-
-    dic_brand={'Administaff HRTools': 'HR', 'H & R Block': 'HR', 'H&R': 'HR', 'H&R BLCOK': 'HR', 'H&R BLOCK': 'HR', 'H&R Block': 'HR', 'H&amp;R Block': 'HR', 'HRBB9': 'HR', 'Intuit': 'Intuit', 'Intuit Inc.': 'Intuit', 'Intuit Inc./BlueHippo': 'Intuit', 'Intuit, Inc.': 'Intuit', 'John Truby Blockbuster': 'Other', 'Teneron/Block Financial Software': 'Other', 'Video Blocks': 'Other', 'by\n    \n    H&R Block': 'HR', 'by\n    \n    Intuit': 'Intuit'}
-    dataset['brand']=dataset['brand'].map(dic_brand)
+    print('Review text normalizado')
     
+    #Clean rank 
+    for i in range(len(dataset)):
+        if dataset['rank'][i].endswith('in Software ('):
+            dataset['rank'][i]=int(dataset['rank'][i].split()[0].replace(',',''))
+        elif dataset['rank'][i].startswith("['>#"): 
+            dataset['rank'][i]=int(dataset['rank'][i].split()[0][4:].replace(',',''))
+        else:
+            dataset['rank'][i]=0
+    print('Rank limpiado')        
 
-    target_map={1: '0', 2: '0', 3: '1', 4: '2', 5: '2'}
+    #Clean price
+    dataset['price']=dataset['price'].astype(double).round(2)
+    print('Price limpiado')
+
+    global target_map
     dataset['__target__'] = dataset[t].map(target_map)
     del dataset[t]
-
     dataset=dataset[~dataset['__target__'].isnull()]
+    print('Target creado')
+    
+    return(dataset)
 
-    print("--- %s seconds ---" % (time.time() - start_time))
 
+def knn(dataset):
+    
     train, test = train_test_split(dataset, test_size=0.2, random_state=42, stratify=dataset[['__target__']])
     print('Train:')
     print(train['__target__'].value_counts())
@@ -146,8 +157,18 @@ def main():
     print(classification_report(testY,predictions))
     print("\nCONFUSION MATRIX:\n")
     print(confusion_matrix(testY, predictions, labels=[0, 1, 2]))
+    
 
 
 if __name__=='__main__':
-    start_time = time.time()
-    main()
+    f='HRBlockIntuitReviewsTrainDev_vLast7.csv'
+    t='overall'
+    dataset=pd.read_csv(f)
+    dataset=preprocess(dataset,t)
+    knn(dataset)
+  
+    
+    '''
+    dic_brand={'Administaff HRTools': 'HR', 'H & R Block': 'HR', 'H&R': 'HR', 'H&R BLCOK': 'HR', 'H&R BLOCK': 'HR', 'H&R Block': 'HR', 'H&amp;R Block': 'HR', 'HRBB9': 'HR', 'Intuit': 'Intuit', 'Intuit Inc.': 'Intuit', 'Intuit Inc./BlueHippo': 'Intuit', 'Intuit, Inc.': 'Intuit', 'John Truby Blockbuster': 'Other', 'Teneron/Block Financial Software': 'Other', 'Video Blocks': 'Other', 'by\n    \n    H&R Block': 'HR', 'by\n    \n    Intuit': 'Intuit'}
+    dataset['brand']=dataset['brand'].map(dic_brand)
+    '''
