1239b99bd6ab5518a1fc2c01f0badd7629f7cfe1
Joel <joelbraortiz@gmail.com>
2022-05-11 13:08:39 +0200

    crear txt

--- LDA/LDA.py
+++ LDA/LDA.py
@@ -24,12 +24,18 @@ no_top_words = 30 #param type: int
 no_top_documents = 10 #param type: int
 
 def display_topics(H, W, feature_names, documents, no_top_words, no_top_documents):
+    f = open('result.txt', 'w')
     for topic_idx, topic in enumerate(H):
         print('Topic %d:' % (topic_idx))
+        f.write('Topic %d:' % (topic_idx))
         print(' '.join([feature_names[i] for i in topic.argsort()[:-no_top_documents]]))
+        f.write("ESCAPEEEEEEE")
+        f.write(' '.join([feature_names[i] for i in topic.argsort()[:-no_top_documents]]))
+        f.write("ESCAPEEEEEEE")
         top_doc_indices = np.argsort(W[:,topic_idx])[::-1][0:no_top_documents]
         for doc_index in top_doc_indices:
             print(documents[doc_index])
+            f.write(documents[doc_index])
 
 tf_vectorizer = CountVectorizer(max_df=0.95, min_df=2, stop_words='english')
 tf = tf_vectorizer.fit_transform(documents)
